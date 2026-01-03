23b37513ad4cee03897c6090e0d57a5a6e284798
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2026-01-03 08:46:42 +0700

    sửa đường dẫn

--- src/services/hoc-sinh/thoi-khoa-bieu.js
+++ src/services/hoc-sinh/thoi-khoa-bieu.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BE + '/hoc-sinh/thoi-khoa-bieu/',
+    baseURL: process.env.NEXT_PUBLIC_BE + '/hoc-sinh/thoi-khoa-bieu',
     headers: {
         "Content-Type": "application/json",
     }
